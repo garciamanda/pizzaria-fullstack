8cb03180fbbc9b585a20c20be85d0ac8e05b1ddc
Matheus Nascimento Braga <124395248+matheusbraganasc@users.noreply.github.com>
2025-03-21 14:32:13 -0300

    modal complementos(incompleto)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,10 @@ import {
   useLocation,
 } from "react-router-dom";
 
-import AdminPage from "./pages/Admin";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home";
 import Feedback from "./pages/Feedback";
 import User from "./pages/User";
-import DashboardPage from "./routes/dashboard/page";
-import { ThemeProvider } from "./contexts/theme-context";
 
 function App() {
   const router = createBrowserRouter([
@@ -33,52 +30,6 @@ function App() {
       ),
     },
     {
-      path: "/admin",
-      element: <AdminPage />,
-      children: [
-        {
-          index: true,
-          element: <DashboardPage />,
-        },
-        {
-          path: "analytics",
-          element: <h1 className="title">Analytics</h1>,
-        },
-        {
-          path: "reports",
-          element: <h1 className="title">Reports</h1>,
-        },
-        {
-          path: "customers",
-          element: <h1 className="title">Customers</h1>,
-        },
-        {
-          path: "new-customer",
-          element: <h1 className="title">New Customer</h1>,
-        },
-        {
-          path: "verified-customers",
-          element: <h1 className="title">Verified Customers</h1>,
-        },
-        {
-          path: "products",
-          element: <h1 className="title">Products</h1>,
-        },
-        {
-          path: "new-product",
-          element: <h1 className="title">New Product</h1>,
-        },
-        {
-          path: "inventory",
-          element: <h1 className="title">Inventory</h1>,
-        },
-        {
-          path: "settings",
-          element: <h1 className="title">Settings</h1>,
-        },
-      ],
-    },
-    {
       path: "/feedback",
       element: (
         <>
@@ -89,11 +40,7 @@ function App() {
     },
   ]);
 
-  return (
-    <ThemeProvider storageKey="theme">
-      <RouterProvider router={router} />
-    </ThemeProvider>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- client/src/components/Modal/ModalProduto.jsx
+++ client/src/components/Modal/ModalProduto.jsx
@@ -1,4 +1,5 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
+import "./ModalProduto.css"; // Importa o arquivo CSS
 
 function ModalProduto({
   modalOpen,
@@ -9,6 +10,8 @@ function ModalProduto({
   preco,
 }) {
   const formRef = useRef(null);
+  const [selectedCount, setSelectedCount] = useState(0);
+  const [message, setMessage] = useState("escolha até 10 opções");
 
   function handleOutsideClick(e) {
     if (formRef.current && !formRef.current.contains(e.target)) {
@@ -24,12 +27,27 @@ function ModalProduto({
 
   useEffect(() => {
     if (modalOpen) {
+      document.body.classList.add("modal-open");
       document.addEventListener("mousedown", handleOutsideClick);
       return () => {
+        document.body.classList.remove("modal-open");
         document.removeEventListener("mousedown", handleOutsideClick);
       };
     }
   }, [modalOpen]);
+
+  const handleCheckboxChange = (e) => {
+    const checked = e.target.checked;
+    setSelectedCount((prevCount) => {
+      const newCount = checked ? prevCount + 1 : prevCount - 1;
+      if (newCount > 10) {
+        setMessage("você escolheu mais que 10 opções");
+      } else {
+        setMessage("escolha até 10 opções");
+      }
+      return newCount;
+    });
+  };
 
   return (
     modalOpen && (
@@ -39,21 +57,158 @@ function ModalProduto({
       >
         <div
           ref={formRef}
-          className="max-w-md mx-auto w-full mt-10 bg-white p-8 border border-gray-300 rounded-lg shadow-lg relative"
+          className="w-[60rem] h-[39rem] mt-10 bg-white border border-gray-300 rounded-lg shadow-lg relative flex"
           onClick={(e) => e.stopPropagation()} // Impede fechamento ao clicar no conteúdo
         >
           <button
             onClick={closeModal}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded absolute top-2 right-1"
+            className="hover:text-red-700 size-0.5 text-black font-bold py-2 px-4 rounded absolute top-[-0rem] right-1"
             title="Fechar"
           >
             &times;
           </button>
-          <div id="produtos">
-            <img src={imagem} alt={nome} />
-            <h2>{nome}</h2>
-            <p>{descricao}</p>
-            <p>R$ {preco}</p>
+          <div className="m-3 flex flex-col justify-center items-center w-1/2" id="imagem">
+            <img className="max-h-full max-w-full h-[22rem] rounded-xl" src={imagem} alt={nome} />
+          </div>
+          <div className="flex flex-col w-2/3 border-l border-gray-300" id="produtos">
+            <div className="bg-gray-200 p-3 h-[4rem] rounded-tr-lg mb-[-20px]" id="nome">
+              <h2 className="text-1xl text-[#666666] font-bold">Produtos</h2>
+            </div>
+            <div className="bg-white p-2 rounded-t-[1.5rem] z-[1] border-t border-gray-300 top-[14.4rem] w-[35rem]" id="descricao">
+              <h2 className="text-2xl font-bold text-[#141414] text-center">{nome}</h2>
+              <p className="text-[13.7px] px-2 text-[#666666] tracking-[-1px] opacity-75">{descricao}</p>
+              <p className="text-[17px] px-2 text-[#141414]">R$ {preco}</p>
+            </div>
+            <div id="ingrediente-escolha" className="bg-gray-200 p-3 h-[5rem] border-t border-b border-gray-300">
+              <h2 className="text-1xl text-[#141414] font-bold">Igredientes adicionais</h2>
+              <p className={`text-[14px] font-serif ${selectedCount > 10 ? 'text-red-500' : 'text-[#666666]'} opacity-80`}>{message}</p>
+            </div>
+            <div id="ingrediente" className="ingrediente border-b border-gray-300 h-[17rem] leading-[2.5rem] p-[1rem] m-[rem] bg-gray-100 overflow-y-auto">
+              <div className="flex justify-between items-center">
+                <label htmlFor="ingrediente1">Cebola</label>
+                <input
+                  type="checkbox"
+                  id="ingrediente1"
+                  name="ingrediente1"
+                  value="ingrediente1"
+                  onChange={handleCheckboxChange}
+                />
+              </div>
+              <div className="flex justify-between items-center">
+                <label htmlFor="ingrediente2">Tomate</label>
+                <input
+                  type="checkbox"
+                  id="ingrediente2"
+                  name="ingrediente2"
+                  value="ingrediente2"
+                  onChange={handleCheckboxChange}
+                />
+              </div>
+              <div className="flex justify-between items-center">
+                <label htmlFor="ingrediente3">Queijo</label>
+                <input
+                  type="checkbox"
+                  id="ingrediente3"
+                  name="ingrediente3"
+                  value="ingrediente3"
+                  onChange={handleCheckboxChange}
+                />
+              </div>
+              <div className="flex justify-between items-center">
+                <label htmlFor="ingrediente4">Presunto</label>
+                <input
+                  type="checkbox"
+                  id="ingrediente4"
+                  name="ingrediente4"
+                  value="ingrediente4"
+                  onChange={handleCheckboxChange}
+                />
+              </div>
+              <div className="flex justify-between items-center">
+                <label htmlFor="ingrediente5">Azeitona</label>
+                <input
+                  type="checkbox"
+                  id="ingrediente5"
+                  name="ingrediente5"
+                  value="ingrediente5"
+                  onChange={handleCheckboxChange}
+                />
+              </div>
+              <div className="flex justify-between items-center">
+                <label htmlFor="ingrediente6">Pimentão</label>
+                <input
+                  type="checkbox"
+                  id="ingrediente6"
+                  name="ingrediente6"
+                  value="ingrediente6"
+                  onChange={handleCheckboxChange}
+                />
+              </div>
+              <div className="flex justify-between items-center">
+                <label htmlFor="ingrediente7">Milho</label>
+                <input
+                  type="checkbox"
+                  id="ingrediente7"
+                  name="ingrediente7"
+                  value="ingrediente7"
+                  onChange={handleCheckboxChange}
+                />
+              </div>
+              <div className="flex justify-between items-center">
+                <label htmlFor="ingrediente8">Bacon</label>
+                <input
+                  type="checkbox"
+                  id="ingrediente8"
+                  name="ingrediente8"
+                  value="ingrediente8"
+                  onChange={handleCheckboxChange}
+                />
+              </div>
+              <div className="flex justify-between items-center">
+                <label htmlFor="ingrediente9">Frango</label>
+                <input
+                  type="checkbox"
+                  id="ingrediente9"
+                  name="ingrediente9"
+                  value="ingrediente9"
+                  onChange={handleCheckboxChange}
+                />
+              </div>
+              <div className="flex justify-between items-center">
+                <label htmlFor="ingrediente10">Calabresa</label>
+                <input
+                  type="checkbox"
+                  id="ingrediente10"
+                  name="ingrediente10"
+                  value="ingrediente10"
+                  onChange={handleCheckboxChange}
+                />
+              </div>
+              <div className="flex justify-between items-center">
+                <label htmlFor="ingrediente11">Palmito</label>
+                <input
+                  type="checkbox"
+                  id="ingrediente11"
+                  name="ingrediente11"
+                  value="ingrediente11"
+                  onChange={handleCheckboxChange}
+                />
+              </div>
+              <div className="flex justify-between items-center">
+                <label htmlFor="ingrediente12">Champignon</label>
+                <input
+                  type="checkbox"
+                  id="ingrediente12"
+                  name="ingrediente12"
+                  value="ingrediente12"
+                  onChange={handleCheckboxChange}
+                />
+              </div>
+            </div>
+            <div id="comprar" className="bg-gray-200 p-9 rounded-br-lg">
+              <button className="bg-[#000000] text-white font-bold py-2 px-4 rounded">Comprar</button>
+
+            </div>
           </div>
         </div>
       </div>
